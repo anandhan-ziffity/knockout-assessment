1a5d7e4ca6f7c9e9018a10343db41777be158cb2
unknown <anandhravi1@gmail.com>
2020-06-28 13:51:44 +0530

    requires js module

---
+++ requireJs/js/config.js
@@ -0,0 +1,8 @@
+requirejs.config({
+    baseUrl: 'js',
+    paths: {
+        knockout: 'https://ajax.aspnetcdn.com/ajax/knockout/knockout-3.1.0',
+        jquery:'https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min',
+        order: 'order'       
+    }
+});
---
+++ requireJs/js/order.js
@@ -0,0 +1,38 @@
+define(['knockout', 'jquery'], function (ko, $) {
+
+        function tableRow(id,name,category,qty,availbaility,discounts) {
+        var self=this;
+       self.id = ko.observable(id);
+       self.name = ko.observable(name);
+       self.category = ko.observable(category);
+       self.qty = ko.observable(qty).extend({ digit: true }).extend({ min: 0});
+       self.availbaility = ko.observable();
+       self.discounts = ko.observableArray();
+      
+   }
+
+   function viewModelAccount() {  
+       var self=this;         
+       self.id=ko.observable();
+       self.name=ko.observable();
+       self.categories= ko.observableArray(["Mobile", "Laptop", "Cricket Bat", "Dress"]);
+       self.tableRows = ko.observableArray([]); 
+       self.answerTableRows =ko.observableArray([]);       
+       self.addNewRow=function(){
+           self.tableRows.push(new tableRow("","","","","",[]
+           ));         
+       }                
+       this.addNewRow();
+       self.submitTable=function(){                    
+           self.answerTableRows(self.tableRows());
+       }               
+       self.removeRow=function(item){
+           self.tableRows.remove(item);
+       }              
+       
+     }      
+     ko.applyBindings(new viewModelAccount());
+     
+     
+
+ });
